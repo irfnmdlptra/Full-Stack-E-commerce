74dfbff1f6daa60f0031dfdabab2df1c68952687
Irfnmdlptra <bangasrat@gmail.com>
2023-04-12 13:22:13 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Category from "./components/Category/Category";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+// import AppContext from "./utils/context";
+
+function App() {
+    return 
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/category:id" element={<Category/> } />
+        <Route path="/product/:id" element={<SingleProduct/>} />
+        </Routes>
+    </BrowserRouter>
+}
+
+export default App;
